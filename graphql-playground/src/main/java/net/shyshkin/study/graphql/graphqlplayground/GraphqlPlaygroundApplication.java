fdc3c5ca7573e6560eae4a0c8650afa5fb43f02f
Art <d.art.shishkin@gmail.com>
2022-10-18 20:24:45 +0300

    5. Project Setup (#1 Section 2: Getting Started With GraphQL)

---
+++ graphql-playground/src/main/java/net/shyshkin/study/graphql/graphqlplayground/GraphqlPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.graphql.graphqlplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlPlaygroundApplication.class, args);
+    }
+
+}
---
+++ graphql-playground/src/test/java/net/shyshkin/study/graphql/graphqlplayground/GraphqlPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.graphql.graphqlplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
