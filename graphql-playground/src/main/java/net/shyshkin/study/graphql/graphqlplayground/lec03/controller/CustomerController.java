ec81cbba8923aa703487087e8465b00c5063b35a
Art <d.art.shishkin@gmail.com>
2022-10-20 12:52:59 +0300

    23. How Fields Are Resolved (#1)

--- graphql-playground/src/main/java/net/shyshkin/study/graphql/graphqlplayground/lec03/controller/CustomerController.java
+++ graphql-playground/src/main/java/net/shyshkin/study/graphql/graphqlplayground/lec03/controller/CustomerController.java
@@ -3,9 +3,11 @@ package net.shyshkin.study.graphql.graphqlplayground.lec03.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.graphql.graphqlplayground.lec03.dto.Customer;
+import net.shyshkin.study.graphql.graphqlplayground.lec03.dto.CustomerOrderDto;
 import net.shyshkin.study.graphql.graphqlplayground.lec03.service.CustomerService;
+import net.shyshkin.study.graphql.graphqlplayground.lec03.service.OrderService;
 import org.springframework.context.annotation.Profile;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 
@@ -16,11 +18,23 @@ import reactor.core.publisher.Flux;
 public class CustomerController {
 
     private final CustomerService service;
+    private final OrderService orderService;
 
-    @QueryMapping
+//    @QueryMapping
+    @SchemaMapping(
+            typeName = "Query"
+    )
     public Flux<Customer> customers() {
         log.debug("fetch all customers");
         return service.getAllCustomers();
+    }
+
+    @SchemaMapping(
+            typeName = "Customer"
+    )
+    public Flux<CustomerOrderDto> orders(Customer customer) {
+        log.debug("fetch all orders of {}", customer);
+        return orderService.ordersByCustomerId(customer.getId());
     }
 
 }
