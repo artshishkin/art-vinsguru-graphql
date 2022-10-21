73a43de7a567c9f8dc1957e469ed1b0d96ef8976
Art <d.art.shishkin@gmail.com>
2022-10-21 16:46:27 +0300

    38.2 Executing Same API With Different Arguments - filteredByAge (#2)

--- graphql-playground/src/test/java/net/shyshkin/study/graphql/graphqlplayground/lec02/controller/MultipleExecutionsTest.java
+++ graphql-playground/src/test/java/net/shyshkin/study/graphql/graphqlplayground/lec02/controller/MultipleExecutionsTest.java
@@ -1,15 +1,20 @@
 package net.shyshkin.study.graphql.graphqlplayground.lec02.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.graphql.graphqlplayground.lec02.dto.AgeRangeFilter;
+import net.shyshkin.study.graphql.graphqlplayground.lec02.service.CustomerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureHttpGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.graphql.test.tester.GraphQlTester;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.then;
 
 @Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -24,6 +29,9 @@ class MultipleExecutionsTest {
 
     @Autowired
     GraphQlTester graphQlTester;
+
+    @SpyBean
+    CustomerService customerService;
 
     @Test
     void multipleCustomersByIdTest() {
@@ -44,6 +52,30 @@ class MultipleExecutionsTest {
         response.path("customer2.city").matchesJson("\"City02\"");
         response.path("customer2.name").matchesJson("\"Customer_02\"");
         response.path("customer2.age").entity(Integer.class).satisfies(age -> assertThat(age).isBetween(18, 60));
+    }
+
+    @Test
+    void multipleFilteredByAgeTest() {
+
+        //given
+        AgeRangeFilter filterLow = AgeRangeFilter.builder()
+                .minAge(18)
+                .maxAge(35)
+                .build();
+        AgeRangeFilter filterHigh = AgeRangeFilter.builder()
+                .minAge(40)
+                .maxAge(60)
+                .build();
+
+        //when
+        GraphQlTester.Response response = graphQlTester.documentName(DOC_LOCATION + "multipleCustomersFiltered")
+                .execute();
+
+        //then
+        response.path("cLow").hasValue();
+        response.path("cHigh").hasValue();
+        then(customerService).should().getCustomersWithinAge(eq(filterLow));
+        then(customerService).should().getCustomersWithinAge(eq(filterHigh));
     }
 
 }
