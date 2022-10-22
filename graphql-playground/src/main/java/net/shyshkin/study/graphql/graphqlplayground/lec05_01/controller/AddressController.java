173168f28e7f65c551d0062f5ccde3cb81b002fe
Art <d.art.shishkin@gmail.com>
2022-10-22 13:02:35 +0300

    47. Data Fetching Environment (#3)

--- graphql-playground/src/main/java/net/shyshkin/study/graphql/graphqlplayground/lec05_01/controller/AddressController.java
+++ graphql-playground/src/main/java/net/shyshkin/study/graphql/graphqlplayground/lec05_01/controller/AddressController.java
@@ -1,5 +1,6 @@
 package net.shyshkin.study.graphql.graphqlplayground.lec05_01.controller;
 
+import graphql.schema.DataFetchingEnvironment;
 import graphql.schema.DataFetchingFieldSelectionSet;
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.graphql.graphqlplayground.lec05_01.dto.Address;
@@ -16,9 +17,11 @@ public class AddressController {
 
     //    @SchemaMapping(typeName = "Customer") //we can skip typeName if we have it in method arguments
     @SchemaMapping
-    public Mono<Address> address(Customer customer, DataFetchingFieldSelectionSet selectionSet) {
+    public Mono<Address> address(Customer customer, DataFetchingFieldSelectionSet selectionSet, DataFetchingEnvironment environment) {
         log.debug("fetch address of {}", customer);
-        log.debug("address: {}", selectionSet.getFields());
+        log.debug("address selection set: {}", selectionSet.getFields());
+        log.debug("address Env document: {}, hash: {}", environment.getDocument(), environment.getDocument().hashCode());
+        log.debug("address Env operation: {}", environment.getOperationDefinition());
         return Mono.fromSupplier(() -> Address.builder()
                 .city("City" + customer.getId())
                 .street("Street" + customer.getId())
--- graphql-playground/src/main/java/net/shyshkin/study/graphql/graphqlplayground/lec05_01/controller/CustomerController.java
+++ graphql-playground/src/main/java/net/shyshkin/study/graphql/graphqlplayground/lec05_01/controller/CustomerController.java
@@ -1,6 +1,6 @@
 package net.shyshkin.study.graphql.graphqlplayground.lec05_01.controller;
 
-import graphql.schema.DataFetchingFieldSelectionSet;
+import graphql.schema.DataFetchingEnvironment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.graphql.graphqlplayground.lec05_01.dto.Customer;
@@ -19,9 +19,11 @@ public class CustomerController {
     private final CustomerService service;
 
     @QueryMapping
-    public Flux<Customer> customers(DataFetchingFieldSelectionSet selectionSet) {
+    public Flux<Customer> customers(DataFetchingEnvironment environment) {
         log.debug("fetch all customers");
-        log.debug("customers: {}", selectionSet.getFields());
+        log.debug("customers SelectionSet: {}", environment.getSelectionSet().getFields());
+        log.debug("customers Env document: {}, hash: {}", environment.getDocument(), environment.getDocument().hashCode());
+        log.debug("customers Env operation: {}", environment.getOperationDefinition());
         return service.getAllCustomers();
     }
 
