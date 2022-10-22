4fc217ef48990d5832dacba6a950b2d9ce1ee4da
Art <d.art.shishkin@gmail.com>
2022-10-22 15:20:15 +0300

    49. Project Setup (#3)

---
+++ graphql-playground/src/main/java/net/shyshkin/study/graphql/graphqlplayground/lec06/service/CustomerService.java
@@ -0,0 +1,37 @@
+package net.shyshkin.study.graphql.graphqlplayground.lec06.service;
+
+import net.shyshkin.study.graphql.graphqlplayground.lec06.dto.Customer;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Service
+@Profile("lec06")
+public class CustomerService {
+
+    private final List<Customer> customers = IntStream.rangeClosed(1, 5)
+            .boxed()
+            .map(this::mockCustomer)
+            .collect(Collectors.toList());
+
+    private final Flux<Customer> flux = Flux.fromIterable(customers);
+
+    public Flux<Customer> getAllCustomers() {
+        return flux;
+    }
+
+    private Customer mockCustomer(Integer id) {
+        return Customer.builder()
+                .id(id)
+                .name(String.format("Customer_%02d", id))
+                .age(ThreadLocalRandom.current().nextInt(18, 60))
+                .city(String.format("City%02d", id))
+                .build();
+    }
+
+}
---
+++ graphql-playground/src/test/java/net/shyshkin/study/graphql/graphqlplayground/lec06/controller/CustomerControllerTest.java
@@ -0,0 +1,81 @@
+package net.shyshkin.study.graphql.graphqlplayground.lec06.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.graphql.graphqlplayground.lec06.dto.Customer;
+import net.shyshkin.study.graphql.graphqlplayground.lec06.dto.CustomerOrderDto;
+import net.shyshkin.study.graphql.graphqlplayground.lec06.service.CustomerService;
+import net.shyshkin.study.graphql.graphqlplayground.lec06.service.OrderService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureHttpGraphQlTester;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.graphql.test.tester.GraphQlTester;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("lec06")
+@AutoConfigureHttpGraphQlTester
+@TestPropertySource(
+        properties = {"spring.graphql.schema.locations: classpath:graphql/lec06"}
+)
+class CustomerControllerTest {
+
+    private static final String DOC_LOCATION = "lec06/";
+
+    @Autowired
+    GraphQlTester graphQlTester;
+
+    @SpyBean
+    CustomerService customerService;
+
+    @SpyBean
+    OrderService orderService;
+
+    @Test
+    void getCustomersWithOrdersTest_shouldCallOrderService() {
+
+        //when
+        GraphQlTester.Response response = graphQlTester.documentName(DOC_LOCATION + "customers")
+                .execute();
+
+        //then
+        response.path("customers")
+                .entityList(Customer.class)
+                .hasSize(5)
+                .satisfies(list -> assertThat(list)
+                        .allSatisfy(c -> assertAll(
+                                        () -> assertThat(c.getCity()).isNull(),
+                                        () -> assertThat(c.getName()).contains("Customer"),
+                                        () -> assertThat(c.getAge()).isBetween(18, 60),
+                                        () -> log.debug("{}", c)
+                                )
+                        )
+                );
+        response.path("customers[0].orders")
+                .entityList(CustomerOrderDto.class)
+                .hasSize(5)
+                .satisfies(orders -> assertThat(orders)
+                        .allSatisfy(o -> assertThat(o).hasNoNullFieldsOrProperties()));
+        response.path("customers[1].orders")
+                .entityList(CustomerOrderDto.class)
+                .hasSize(5)
+                .satisfies(orders -> assertThat(orders)
+                        .allSatisfy(o -> assertThat(o).hasNoNullFieldsOrProperties()));
+        response.path("customers[4].orders")
+                .entityList(CustomerOrderDto.class)
+                .hasSize(0);
+
+        then(customerService).should().getAllCustomers();
+        then(orderService).should(times(5)).ordersByCustomerId(any());
+    }
+
+}
