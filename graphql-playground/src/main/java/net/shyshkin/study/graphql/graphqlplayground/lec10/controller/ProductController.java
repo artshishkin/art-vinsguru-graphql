1483e1b13a04fba892ed6c569ab238ac3cfa4f58
Art <d.art.shishkin@gmail.com>
2022-10-23 20:43:51 +0300

    65. GraphQL Interface - Implementation (#3)

---
+++ graphql-playground/src/main/java/net/shyshkin/study/graphql/graphqlplayground/lec10/controller/ProductController.java
@@ -0,0 +1,38 @@
+package net.shyshkin.study.graphql.graphqlplayground.lec10.controller;
+
+import net.shyshkin.study.graphql.graphqlplayground.lec10.dto.Book;
+import net.shyshkin.study.graphql.graphqlplayground.lec10.dto.Electronics;
+import net.shyshkin.study.graphql.graphqlplayground.lec10.dto.Fruit;
+import org.springframework.context.annotation.Profile;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDate;
+
+@Controller
+@Profile("lec10")
+public class ProductController {
+
+    @QueryMapping
+    public Flux<Object> products(){
+        return Flux.just(
+                Fruit.builder()
+                        .description("banana")
+                        .price(10)
+                        .expiryDate(LocalDate.now().plusDays(14))
+                        .build(),
+                Electronics.builder()
+                        .description("iPhone XS")
+                        .brand("APPLE")
+                        .price(1000)
+                        .build(),
+                Book.builder()
+                        .description("Lord of Rings")
+                        .author("Tolkien")
+                        .price(100)
+                        .build()
+        );
+    }
+
+}
---
+++ graphql-playground/src/main/java/net/shyshkin/study/graphql/graphqlplayground/lec10/dto/Fruit.java
@@ -0,0 +1,22 @@
+package net.shyshkin.study.graphql.graphqlplayground.lec10.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Fruit {
+
+    private final UUID id = UUID.randomUUID();
+    private String description;
+    private Integer price;
+    private LocalDate expiryDate;
+
+}
