50b7b01c282f999aa4a9893c10ab8294b047bc98
Art <d.art.shishkin@gmail.com>
2022-10-24 16:59:56 +0300

    74. Operation Caching - Implementation (#3)

---
+++ graphql-playground/src/main/java/net/shyshkin/study/graphql/graphqlplayground/lec12/config/OperationCachingConfig.java
@@ -0,0 +1,47 @@
+package net.shyshkin.study.graphql.graphqlplayground.lec12.config;
+
+import graphql.ExecutionInput;
+import graphql.execution.preparsed.PreparsedDocumentEntry;
+import graphql.execution.preparsed.PreparsedDocumentProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
+@Slf4j
+@Profile("lec12")
+@Configuration
+public class OperationCachingConfig {
+
+    @Bean
+    GraphQlSourceBuilderCustomizer sourceBuilderCustomizer() {
+        return c -> c.configureGraphQl(
+                b -> b.preparsedDocumentProvider(cachedPreparsedDocumentProvider())
+        );
+    }
+
+    @Bean
+    PreparsedDocumentProvider cachedPreparsedDocumentProvider() {
+        Map<String, PreparsedDocumentEntry> cache = new ConcurrentHashMap<>();
+        return new PreparsedDocumentProvider() {
+            @Override
+            public PreparsedDocumentEntry getDocument(ExecutionInput executionInput, Function<ExecutionInput, PreparsedDocumentEntry> parseAndValidateFunction) {
+                return cache.computeIfAbsent(
+                        executionInput.getQuery(),
+                        q -> {
+                            log.debug("Not found in cache: {}", q);
+                            PreparsedDocumentEntry preparsedDocumentEntry = parseAndValidateFunction.apply(executionInput);
+                            log.debug("Caching: {}", preparsedDocumentEntry);
+                            return preparsedDocumentEntry;
+                        }
+                );
+            }
+        };
+    }
+
+}
