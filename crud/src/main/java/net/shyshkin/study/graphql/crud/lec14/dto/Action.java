3a0b22df2e70e3ae7739fe88b68d48677773f59c
Art <d.art.shishkin@gmail.com>
2022-10-25 17:26:58 +0300

    93. Subscription - Event Service (#5)

---
+++ crud/src/main/java/net/shyshkin/study/graphql/crud/lec14/dto/Action.java
@@ -0,0 +1,7 @@
+package net.shyshkin.study.graphql.crud.lec14.dto;
+
+public enum Action {
+
+    CREATED, UPDATED, DELETED
+
+}
---
+++ crud/src/main/java/net/shyshkin/study/graphql/crud/lec14/service/CustomerEventService.java
@@ -0,0 +1,26 @@
+package net.shyshkin.study.graphql.crud.lec14.service;
+
+import net.shyshkin.study.graphql.crud.lec14.dto.CustomerEvent;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+@Service
+public class CustomerEventService {
+
+    private final Sinks.Many<CustomerEvent> sink = Sinks
+            .many()
+            .multicast()
+            .onBackpressureBuffer();
+
+    private final Flux<CustomerEvent> flux = sink.asFlux();
+
+    public Flux<CustomerEvent> subscribe() {
+        return flux;
+    }
+
+    public void emitEvent(CustomerEvent event) {
+        sink.tryEmitNext(event);
+    }
+
+}
