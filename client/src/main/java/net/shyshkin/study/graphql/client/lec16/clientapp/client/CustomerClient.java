23f094d5e2a4e2ddab75edf0ce91f507d8ab4f39
Art <d.art.shishkin@gmail.com>
2022-10-27 17:34:52 +0300

    115. GraphQL WebClient (#7)

---
+++ client/src/main/java/net/shyshkin/study/graphql/client/lec16/clientapp/client/CustomerClient.java
@@ -0,0 +1,25 @@
+package net.shyshkin.study.graphql.client.lec16.clientapp.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.graphql.client.ClientGraphQlResponse;
+import org.springframework.graphql.client.HttpGraphQlClient;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomerClient {
+
+    private final HttpGraphQlClient client;
+
+    public CustomerClient(@Value("${customer.service.url}") String baseUrl) {
+        this.client = HttpGraphQlClient.builder()
+                .webClient(b -> b.baseUrl(baseUrl))
+                .build();
+    }
+
+    public Mono<ClientGraphQlResponse> rawQuery(String query) {
+        return this.client.document(query)
+                .execute();
+    }
+
+}
