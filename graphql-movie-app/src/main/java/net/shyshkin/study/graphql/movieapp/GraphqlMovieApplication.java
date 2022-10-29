dc79873a4a63a5f4e02d4d368859bc7c1289ca4c
Art <d.art.shishkin@gmail.com>
2022-10-29 20:31:44 +0300

    155. Project Setup (#10)

---
+++ graphql-movie-app/src/main/java/net/shyshkin/study/graphql/movieapp/GraphqlMovieApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.graphql.movieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlMovieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlMovieApplication.class, args);
+    }
+
+}
---
+++ graphql-movie-app/src/test/java/net/shyshkin/study/graphql/movieapp/GraphqlMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.graphql.movieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlMovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
