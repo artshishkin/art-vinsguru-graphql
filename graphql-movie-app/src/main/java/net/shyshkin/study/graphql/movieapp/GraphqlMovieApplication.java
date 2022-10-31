5bf2c65cec303373087e98cc74b80cd33986726b
Art <d.art.shishkin@gmail.com>
2022-10-31 19:55:01 +0200

    9.1 OAuth2 resource server dependency (#9 GraphQL Security with Keycloak)

--- graphql-movie-app/src/main/java/net/shyshkin/study/graphql/movieapp/GraphqlMovieApplication.java
+++ graphql-movie-app/src/main/java/net/shyshkin/study/graphql/movieapp/GraphqlMovieApplication.java
@@ -2,8 +2,9 @@ package net.shyshkin.study.graphql.movieapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ReactiveSecurityAutoConfiguration.class})
 public class GraphqlMovieApplication {
 
     public static void main(String[] args) {
