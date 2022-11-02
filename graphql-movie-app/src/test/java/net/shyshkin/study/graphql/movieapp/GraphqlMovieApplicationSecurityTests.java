584aa95f3b7c3a492c334302566aaf49b3731abe
Art <d.art.shishkin@gmail.com>
2022-11-02 15:11:30 +0200

    9.14 User Security Tests (#9)

--- graphql-movie-app/src/test/java/net/shyshkin/study/graphql/movieapp/GraphqlMovieApplicationSecurityTests.java
+++ graphql-movie-app/src/test/java/net/shyshkin/study/graphql/movieapp/GraphqlMovieApplicationSecurityTests.java
@@ -922,6 +922,419 @@ class GraphqlMovieApplicationSecurityTests extends BaseTest {
         }
     }
 
+    @Nested
+    class UserTests {
+
+        @BeforeEach
+        void setUp() {
+            graphQlSecuredTester = graphQlTester
+                    .mutate()
+                    .headers(httpHeaders -> httpHeaders.setBearerAuth(getJwtAccessToken(APP_USER_USERNAME, APP_USER_PASSWORD)))
+                    .build();
+        }
+
+        @Nested
+        class QueryTests {
+
+            @Test
+            void getUserProfileById_absent() {
+                //given
+                Integer userId = 1000;
+
+                //when
+                GraphQlTester.Response response = graphQlSecuredTester.documentName("queries")
+                        .operationName("getUserProfileCut")
+                        .variable("userId", userId)
+                        .execute();
+
+                //then
+                response.errors()
+                        .satisfy(errs -> assertThat(errs).hasSize(1)
+                                .satisfies(
+                                        error -> assertAll(
+                                                () -> assertThat(error.getErrorType()).isEqualTo(ErrorType.FORBIDDEN),
+                                                () -> assertThat(error.getMessage()).isEqualTo("Forbidden"),
+                                                () -> assertThat(error.getPath()).isEqualTo("userProfile")
+                                        ),
+                                        Index.atIndex(0)
+                                )
+                        );
+
+
+                then(customerClient).shouldHaveNoInteractions();
+                then(movieClient).shouldHaveNoInteractions();
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+            @Test
+            void getUserProfileByIdCut_present() {
+                //given
+                Integer userId = 1;
+
+                //when
+                graphQlSecuredTester.documentName("queries")
+                        .operationName("getUserProfileCut")
+                        .variable("userId", userId)
+                        .execute()
+
+                        //then
+                        .errors()
+                        .satisfy(errs -> assertThat(errs).hasSize(1)
+                                .satisfies(
+                                        error -> assertAll(
+                                                () -> assertThat(error.getErrorType()).isEqualTo(ErrorType.FORBIDDEN),
+                                                () -> assertThat(error.getMessage()).isEqualTo("Forbidden"),
+                                                () -> assertThat(error.getPath()).isEqualTo("userProfile")
+                                        ),
+                                        Index.atIndex(0)
+                                )
+                        );
+
+                then(customerClient).shouldHaveNoInteractions();
+                then(movieClient).shouldHaveNoInteractions();
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+            @Test
+            void getUserProfileByIdFull_present() {
+                //given
+                Integer userId = 1;
+
+                //when
+                GraphQlTester.Response response = graphQlSecuredTester.documentName("queries")
+                        .operationName("getUserProfileFull")
+                        .variable("userId", userId)
+                        .execute();
+
+                //then
+                response.errors()
+                        .satisfy(errs -> assertThat(errs).hasSize(1)
+                                .satisfies(
+                                        error -> assertAll(
+                                                () -> assertThat(error.getErrorType()).isEqualTo(ErrorType.FORBIDDEN),
+                                                () -> assertThat(error.getMessage()).isEqualTo("Forbidden"),
+                                                () -> assertThat(error.getPath()).isEqualTo("userProfile")
+                                        ),
+                                        Index.atIndex(0)
+                                )
+                        );
+
+                then(customerClient).shouldHaveNoInteractions();
+                then(movieClient).shouldHaveNoInteractions();
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+            @Test
+            void getMovieDetails_absent() {
+                //given
+                Integer movieId = 10000;
+
+                //when
+                graphQlSecuredTester.documentName("queries")
+                        .operationName("getMovieDetailsCut")
+                        .variable("movieId", movieId)
+                        .execute()
+
+                        //then
+                        .path("movieDetails").valueIsNull();
+
+                then(customerClient).shouldHaveNoInteractions();
+                then(movieClient).should().getMoviesByIds(any());
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+            @Test
+            void getMovieDetailsCut_present() {
+                //given
+                Integer movieId = 1;
+
+                //when
+                graphQlSecuredTester.documentName("queries")
+                        .operationName("getMovieDetailsCut")
+                        .variable("movieId", movieId)
+                        .execute()
+
+                        //then
+                        .path("movieDetails").hasValue()
+                        .entity(Movie.class)
+                        .satisfies(movie -> assertThat(movie).hasNoNullFieldsOrProperties());
+
+                then(customerClient).shouldHaveNoInteractions();
+                then(movieClient).should().getMoviesByIds(any());
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+            @Test
+            void getMovieDetailsFull_present() {
+                //given
+                Integer movieId = 1;
+
+                //when
+                GraphQlTester.Response response = graphQlSecuredTester.documentName("queries")
+                        .operationName("getMovieDetailsFull")
+                        .variable("movieId", movieId)
+                        .execute();
+
+
+                //then
+                response.errors()
+                        .satisfy(errs -> assertThat(errs).hasSize(1)
+                                .satisfies(
+                                        error -> assertAll(
+                                                () -> assertThat(error.getErrorType()).isEqualTo(ErrorType.FORBIDDEN),
+                                                () -> assertThat(error.getMessage()).isEqualTo("Forbidden"),
+                                                () -> assertThat(error.getPath()).isEqualTo("movieDetails.reviews")
+                                        ),
+                                        Index.atIndex(0)
+                                )
+                        );
+
+                then(customerClient).shouldHaveNoInteractions();
+                then(movieClient).should().getMoviesByIds(any());
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+            @Test
+            void getMoviesByGenreTest() {
+                //given
+                Genre genre = Genre.ACTION;
+
+                //when
+                GraphQlTester.Response response = graphQlSecuredTester.documentName("queries")
+                        .operationName("getMoviesByGenre")
+                        .variable("genre", genre)
+                        .execute();
+
+                //then
+                response.path("moviesByGenre").hasValue()
+                        .entityList(Movie.class)
+                        .satisfies(movies -> assertThat(movies)
+                                .allSatisfy(movie -> assertThat(movie).hasNoNullFieldsOrProperties()));
+                then(customerClient).shouldHaveNoInteractions();
+                then(movieClient).should().getMovieRecommendationByGenre(eq(genre));
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+        }
+
+        @Nested
+        class MutationTests {
+
+            @Test
+            void updateUserProfile_absent() {
+                //given
+                CustomerInput customerInput = new CustomerInput() {{
+                    setId(1000);
+                    setName("Abs");
+                    setFavoriteGenre(Genre.ADVENTURE);
+                }};
+
+                //when
+                graphQlSecuredTester.documentName("mutations")
+                        .operationName("updateUserProfileCut")
+                        .variable("customerInput", customerInput)
+                        .execute()
+
+                        //then
+                        .path("result").valueIsNull();
+
+                then(customerClient).should().updateCustomer(eq(customerInput));
+                then(customerClient).shouldHaveNoMoreInteractions();
+                then(movieClient).shouldHaveNoInteractions();
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+            @Test
+            void updateUserProfileCut_present() {
+                //given
+                CustomerInput customerInput = new CustomerInput() {{
+                    setId(1);
+                    setName("Arina");
+                    setFavoriteGenre(Genre.ADVENTURE);
+                }};
+
+                //when
+                graphQlSecuredTester.documentName("mutations")
+                        .operationName("updateUserProfileCut")
+                        .variable("customerInput", customerInput)
+                        .execute()
+
+                        //then
+                        .path("result").hasValue()
+                        .path("result.id").entity(Integer.class).isEqualTo(1)
+                        .path("result.name").entity(String.class).isEqualTo("Arina")
+                        .path("result.favoriteGenre").entity(Genre.class).isEqualTo(Genre.ADVENTURE)
+                        .path("result.watchList").pathDoesNotExist()
+                        .path("result.recommended").pathDoesNotExist();
+
+                then(customerClient).should().updateCustomer(eq(customerInput));
+                then(customerClient).shouldHaveNoMoreInteractions();
+                then(movieClient).shouldHaveNoInteractions();
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+            @Test
+            void updateUserProfileFull_present() {
+                //given
+                CustomerInput customerInput = new CustomerInput() {{
+                    setId(2);
+                    setName("Mike");
+                    setFavoriteGenre(Genre.COMEDY);
+                }};
+
+                //when
+                GraphQlTester.Response response = graphQlSecuredTester.documentName("mutations")
+                        .operationName("updateUserProfileFull")
+                        .variable("customerInput", customerInput)
+                        .execute();
+
+                //then
+                response.path("result").hasValue()
+                        .path("result.id").entity(Integer.class).isEqualTo(2)
+                        .path("result.name").entity(String.class).isEqualTo("Mike")
+                        .path("result.favoriteGenre").entity(Genre.class).isEqualTo(Genre.COMEDY);
+                response.path("result.watchList")
+                        .hasValue()
+                        .entityList(Movie.class)
+                        .satisfies(movies -> assertThat(movies)
+                                .allSatisfy(m -> assertThat(m).hasNoNullFieldsOrProperties()));
+                response.path("result.recommended")
+                        .hasValue()
+                        .entityList(Movie.class)
+                        .hasSizeGreaterThan(1)
+                        .satisfies(movies -> assertThat(movies)
+                                .allSatisfy(m -> assertThat(m).hasNoNullFieldsOrProperties()));
+
+                then(customerClient).should().updateCustomer(eq(customerInput));
+                then(customerClient).shouldHaveNoMoreInteractions();
+                then(movieClient).should().getMoviesByIds(any());
+                then(movieClient).should().getMovieRecommendationByGenre(any());
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+            @Test
+            @DisplayName("Even if the customer does not exist adding movie to his list will be success???")
+            void addMovieToUserWatchList_absentUser() {
+                //given
+                WatchListInput watchListInput = new WatchListInput() {{
+                    setCustomerId(1000);
+                    setMovieId(12);
+                }};
+
+                //when
+                graphQlSecuredTester.documentName("mutations")
+                        .operationName("addMovieToUserWatchListSimple")
+                        .variable("watchListInput", watchListInput)
+                        .execute()
+
+                        //then
+                        .path("result").hasValue()
+                        .path("result.status").entity(Status.class).isEqualTo(Status.SUCCESS)
+                        .path("result.watchList").pathDoesNotExist();
+
+                then(customerClient).should().addMovieToCustomerWatchlist(eq(watchListInput));
+                then(customerClient).shouldHaveNoMoreInteractions();
+                then(movieClient).shouldHaveNoInteractions();
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+            @Test
+            @DisplayName("If customer exists and movie exists then adding movie to the customer's watchlist should be correct")
+            void addMovieToUserWatchListSimple() {
+                //given
+                WatchListInput watchListInput = new WatchListInput() {{
+                    setCustomerId(3);
+                    setMovieId(12);
+                }};
+
+                //when
+                graphQlSecuredTester.documentName("mutations")
+                        .operationName("addMovieToUserWatchListSimple")
+                        .variable("watchListInput", watchListInput)
+                        .execute()
+
+                        //then
+                        .path("result").hasValue()
+                        .path("result.status").entity(Status.class).isEqualTo(Status.SUCCESS)
+                        .path("result.watchList").pathDoesNotExist();
+
+                then(customerClient).should().addMovieToCustomerWatchlist(eq(watchListInput));
+                then(customerClient).shouldHaveNoMoreInteractions();
+                then(movieClient).shouldHaveNoInteractions();
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+            @Test
+            @DisplayName("When everything is OK then we should see movie in watchlist")
+            void addMovieToUserWatchListFull() {
+                //given
+                int movieId = 124;
+                WatchListInput watchListInput = new WatchListInput() {{
+                    setCustomerId(2);
+                    setMovieId(movieId);
+                }};
+
+                //when
+                GraphQlTester.Response response = graphQlSecuredTester.documentName("mutations")
+                        .operationName("addMovieToUserWatchListFull")
+                        .variable("watchListInput", watchListInput)
+                        .execute();
+
+                //then
+                response.path("result").hasValue();
+
+                response.path("result.status").entity(Status.class).isEqualTo(Status.SUCCESS);
+
+                response.path("result.watchList")
+                        .hasValue()
+                        .entityList(Movie.class)
+                        .satisfies(movies -> assertThat(movies)
+                                .allSatisfy(m -> assertThat(m).hasNoNullFieldsOrProperties())
+                                .anySatisfy(m -> assertThat(m.getId()).isEqualTo(movieId))
+                        );
+                then(customerClient).should().addMovieToCustomerWatchlist(eq(watchListInput));
+                then(customerClient).shouldHaveNoMoreInteractions();
+                then(movieClient).should().getMoviesByIds(any());
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+            @Test
+            @DisplayName("Even if the movie does not exist adding it to watchlist will be success but movie will not return in response")
+            void addMovieToUserWatchList_absentMovie() {
+                //given
+                int movieId = 12000;
+                WatchListInput watchListInput = new WatchListInput() {{
+                    setCustomerId(3);
+                    setMovieId(movieId);
+                }};
+
+                //when
+                GraphQlTester.Response response = graphQlSecuredTester.documentName("mutations")
+                        .operationName("addMovieToUserWatchListFull")
+                        .variable("watchListInput", watchListInput)
+                        .execute();
+
+                //then
+                response.path("result").hasValue();
+
+                response.path("result.status").entity(Status.class).isEqualTo(Status.SUCCESS);
+
+                response.path("result.watchList")
+                        .hasValue()
+                        .entityList(Movie.class)
+                        .satisfies(movies -> assertThat(movies)
+                                .allSatisfy(m -> assertThat(m).hasNoNullFieldsOrProperties())
+                                .allSatisfy(m -> assertThat(m.getId()).isNotEqualTo(movieId))
+                        );
+
+                then(customerClient).should().addMovieToCustomerWatchlist(eq(watchListInput));
+                then(customerClient).shouldHaveNoMoreInteractions();
+                then(movieClient).should().getMoviesByIds(any());
+                then(reviewClient).shouldHaveNoInteractions();
+            }
+
+        }
+    }
+
     @DynamicPropertySource
     static void appProperties(DynamicPropertyRegistry registry) {
         registry.add(
