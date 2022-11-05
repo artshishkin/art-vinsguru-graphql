14c46e3060d5a32a6f2ddae8ec8e433048fb0a61
Art <d.art.shishkin@gmail.com>
2022-11-05 12:01:13 +0200

    11.9 Mutations over RSocket CLI (RSC) and RSocket IntelliJ Plugin (#11)

--- graphql-rsocket-movie-app/src/test/resources/req.sh
+++ graphql-rsocket-movie-app/src/test/resources/req.sh
@@ -5,12 +5,18 @@ rsc --request --route 'graphql' -d '{\"query\": \"query{ping}\"}' --debug tcp://
 rsc --request --route 'graphql' --load ./query-ping.json --debug tcp://localhost:6565
 
 ### get user profile
-rsc --request --route 'graphql' --load ./query-user-profile.json --debug tcp://localhost:6565### get user profile
+rsc --request --route 'graphql' --load ./query-user-profile.json --debug tcp://localhost:6565
 
 ### get movie details
 rsc --request --route 'graphql' --load ./query-movie-details.json --debug tcp://localhost:6565
 
 ### get movies by genre
 rsc --request --route 'graphql' --load ./query-movies-by-genre.json --debug tcp://localhost:6565
+
+### mutation update user profile
+rsc --request --route 'graphql' --load ./mutation-update-user-profile.json --debug tcp://localhost:6565
+
+### mutation add to watchlist
+rsc --request --route 'graphql' --load ./mutation-add-to-watchlist.json --debug tcp://localhost:6565
 
 
