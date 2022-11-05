a36aff1e91552c509b1461f9a9f7ea095be9dc88
Art <d.art.shishkin@gmail.com>
2022-11-05 11:39:24 +0200

    11.7 Query Movie Details (#11)

--- graphql-rsocket-movie-app/src/test/resources/req.sh
+++ graphql-rsocket-movie-app/src/test/resources/req.sh
@@ -5,4 +5,7 @@ rsc --request --route 'graphql' -d '{\"query\": \"query{ping}\"}' --debug tcp://
 rsc --request --route 'graphql' --load ./query-ping.json --debug tcp://localhost:6565
 
 ### get user profile
-rsc --request --route 'graphql' --load ./query-user-profile.json --debug tcp://localhost:6565
+rsc --request --route 'graphql' --load ./query-user-profile.json --debug tcp://localhost:6565### get user profile
+
+### get movie details
+rsc --request --route 'graphql' --load ./query-movie-details.json --debug tcp://localhost:6565
