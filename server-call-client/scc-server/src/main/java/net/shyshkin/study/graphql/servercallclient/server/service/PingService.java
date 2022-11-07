1294484e8936a28ba850ca6833936f7e1ecbda2f
Art <d.art.shishkin@gmail.com>
2022-11-07 22:16:51 +0200

    12.11 Ping Service (#12)

---
+++ server-call-client/scc-server/src/main/java/net/shyshkin/study/graphql/servercallclient/server/service/PingService.java
@@ -0,0 +1,31 @@
+package net.shyshkin.study.graphql.servercallclient.server.service;
+
+import lombok.RequiredArgsConstructor;
+import net.shyshkin.study.graphql.servercallclient.server.client.CustomRSocketGraphQlClientBuilder;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class PingService {
+
+    private final RSocketRequesterManager rSocketRequesterManager;
+
+    public Mono<String> ping(UUID requesterId) {
+
+        Optional<RSocketRequester> requesterOptional = rSocketRequesterManager.getRequester(requesterId);
+
+        return Mono.justOrEmpty(requesterOptional)
+                .map(requester -> new CustomRSocketGraphQlClientBuilder(requester).build())
+                .flatMap(client -> client
+                                .document("query{ping}")
+                                .retrieve("ping")
+                                .toEntity(String.class)
+                );
+    }
+
+}
