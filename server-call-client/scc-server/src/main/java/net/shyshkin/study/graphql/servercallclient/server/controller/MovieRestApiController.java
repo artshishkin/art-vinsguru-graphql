2aa77e03132292959c71b3eb3d5ba472ea0c9b72
Art <d.art.shishkin@gmail.com>
2022-11-08 22:15:34 +0200

    12.17 Passing Client Id through custom Http Header (#12)

--- server-call-client/scc-server/src/main/java/net/shyshkin/study/graphql/servercallclient/server/controller/MovieRestApiController.java
+++ server-call-client/scc-server/src/main/java/net/shyshkin/study/graphql/servercallclient/server/controller/MovieRestApiController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import net.shyshkin.study.graphql.servercallclient.common.dto.Movie;
 import net.shyshkin.study.graphql.servercallclient.server.service.MovieService;
 import net.shyshkin.study.graphql.servercallclient.server.service.PingService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
@@ -20,13 +17,13 @@ public class MovieRestApiController {
     private final PingService pingService;
     private final MovieService movieService;
 
-    @GetMapping("{requesterId}/ping")
-    Mono<String> ping(@PathVariable UUID requesterId) {
+    @GetMapping("ping")
+    Mono<String> ping(@RequestHeader("X-Client-Id") UUID requesterId) {
         return pingService.ping(requesterId);
     }
 
-    @GetMapping("{requesterId}/movies/{movieId}")
-    Mono<Movie> getMovieDetails(@PathVariable UUID requesterId, @PathVariable Integer movieId) {
+    @GetMapping("movies/{movieId}")
+    Mono<Movie> getMovieDetails(@RequestHeader("X-Client-Id") UUID requesterId, @PathVariable Integer movieId) {
         return movieService.getMovieDetailsCut(requesterId, movieId);
     }
 
