142ba0a06fb7d6120ab7d237078af7e73abb9456
Art <d.art.shishkin@gmail.com>
2022-11-09 09:56:26 +0200

    12.20 @JsonInclude(JsonInclude.Include.NON_NULL) (#12)

--- server-call-client/scc-server/src/main/java/net/shyshkin/study/graphql/servercallclient/server/dto/MovieDetails.java
+++ server-call-client/scc-server/src/main/java/net/shyshkin/study/graphql/servercallclient/server/dto/MovieDetails.java
@@ -1,5 +1,6 @@
 package net.shyshkin.study.graphql.servercallclient.server.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -11,6 +12,7 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class MovieDetails extends Movie {
     List<Review> reviews;
 }
