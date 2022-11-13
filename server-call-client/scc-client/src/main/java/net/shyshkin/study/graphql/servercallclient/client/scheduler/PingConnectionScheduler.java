3ae49e8a4345ea2d2cd129f78cf584419d6243ac
Art <d.art.shishkin@gmail.com>
2022-11-13 12:24:26 +0200

    16.2 Ping Server periodically to establish connection [skip ci] (#16, #12)

---
+++ server-call-client/scc-client/src/main/java/net/shyshkin/study/graphql/servercallclient/client/scheduler/PingConnectionScheduler.java
@@ -0,0 +1,27 @@
+package net.shyshkin.study.graphql.servercallclient.client.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Slf4j
+@EnableScheduling
+@Configuration
+@RequiredArgsConstructor
+public class PingConnectionScheduler {
+
+    private final RSocketRequester requester;
+
+    @Scheduled(fixedDelay = 1000)
+    public void fireAndForgetServerCall() {
+        log.debug("Sending `hello` message to connect to server periodically");
+        requester.route("hello")
+                .data("Art")
+                .send()
+                .subscribe();
+    }
+
+}
